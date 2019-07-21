a20a007cc03225a95c07851ecb2f8906cb57c9e5
gagharutyunyan <gagharut@gmail.com>
2019-07-21 12:11:30 +0300

    Updates

---
+++ precache-manifest.223edb1d23a068dacdb60cfa87f00b13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e65a4bf428f0a93e1882a41f5e5b14de",
+    "url": "/gagharutyunyan/films/index.html"
+  },
+  {
+    "revision": "a15c0ea3e31ed9a7976f",
+    "url": "/gagharutyunyan/films/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/gagharutyunyan/films/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "a15c0ea3e31ed9a7976f",
+    "url": "/gagharutyunyan/films/static/js/main.d173c0a8.chunk.js"
+  },
+  {
+    "revision": "d26338526cd09048d68a",
+    "url": "/gagharutyunyan/films/static/js/runtime~main.672f0102.js"
+  }
+]);
