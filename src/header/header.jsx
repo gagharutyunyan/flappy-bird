083c567757d023ed08d9b83a2d3b47c207376949
gagharutyunyan <gagharut@gmail.com>
2019-07-21 12:13:38 +0300

    adding HEADER component

---
+++ src/header/header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Header = () => (
+	<header>
+		<div className="slider">
+			<div className="about">
+				<div className="btn play">play</div>
+				<div className="about-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, repudiandae quis.</div>
+			</div>
+			<div className="toogle">
+				<div className="btn toogle-left">left</div>
+				<div className="btn play">play</div>
+				<div className="btn toogle-right">right</div>
+				<div className="toogle-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, repudiandae quis.</div>
+			</div>
+		</div>
+	</header>
+) 
+
+export default Header;
